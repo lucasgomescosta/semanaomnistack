cfaec97d72e5a30c8f2a7a4fdb6eefaaf2a17eeb
lucasgomescosta <lucasgomescosta@users.noreply.github.com>
2020-01-16 23:28:33 -0400

    web

---
+++ web/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './global.css';
+import './App.css';
+import './Sidebbar.css';
+
+function App() {
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form action="">
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Usuário do Github</label>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+             <div class="input-block">
+                <label htmlFor="latitude">Latitude</label>
+                <input name="latitude" id="latitude" required/>
+             </div>
+             
+             <div class="input-block">
+                <label htmlFor="longitude">Usuário do Github</label>
+                <input name="longitude" id="longitude" required/>
+             </div> 
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+
+
+      </aside>
+
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
